58fdf68d72b1218e6e4ff5c1070bab93a9a83ae3
Hamza Asal <69694292+HamzaAl-Asal@users.noreply.github.com>
2024-06-12 17:28:48 +0300

    Update JWTAuthenticationOptions.cs

--- Modules/HappyCompany.JwtAuthentication/Options/JWTAuthenticationOptions.cs
+++ Modules/HappyCompany.JwtAuthentication/Options/JWTAuthenticationOptions.cs
@@ -3,8 +3,5 @@
     public class JWTAuthenticationOptions
     {
         public string SecretKey { get; set; }
-        public bool ValidateIssuerSigningKey { get; set; }
-        public bool ValidateIssuer { get; set; }
-        public bool ValidateAudience { get; set; }
     }
 }
